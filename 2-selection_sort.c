004998a6690b3468b87a1a7cda66d9afcd8868be
HarpII <walesii19@gmail.com>
2023-01-21 15:42:53 +0000

    initial commit

---
+++ 2-selection_sort.c
@@ -0,0 +1,40 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array using
+ * selection sort method
+ * @array: array to be sorted
+ * @size: size of array
+ *
+ * Return: void
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t lowestValueIndex = 0, temp = 0;
+	size_t i, j;
+
+	if (size < 2)
+	{
+		return
+	}
+
+	for (i = 0; i < size - 1; i++)
+	{
+		lowestValueIndex = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[lowestValueIndex])
+			{
+				lowestValueIndex = j;
+			}
+		}
+
+		if (lowestValueIndex != i)
+		{
+			temp = array[i];
+			array[i] = array[lowestValueIndex];
+			array[lowestValueIndex] = temp;
+			print_array(array, size);
+		}
+	}
+}
